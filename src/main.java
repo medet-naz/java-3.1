3fc760ccc0bdb0a906c9465c57e5de258f82c034
Binura Nazarbekova <11k.binurae@gmail.com>
2021-04-06 15:48:31 +0600

    src added

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int ticket = 2000;
+        int bonus = 20;
+        int miles = ticket / bonus;
+
+        System.out.println(miles);
+    }
+}
